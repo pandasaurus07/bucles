1ab67974d1286919a3e8d3552fac4b088279fd3a
pandasaurus07 <114919763+pandasaurus07@users.noreply.github.com>
2023-02-01 13:45:15 +0100

    Create lista.py

---
+++ lista.py
@@ -0,0 +1,17 @@
+####################
+# ¿Qué realiza el código? #
+####################
+from microbit import*
+
+i = 0
+lista = ["A","B","C","D","E"]
+
+while True:
+    if button_a.was_pressed():
+        i = i - 1
+        sleep(500)
+    elif button_b.was_pressed():
+        i = i + 1
+        sleep(500)
+    if
+    
