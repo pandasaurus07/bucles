c7bb3ac69ec3a023b0c821076259bc1c8ec5dd5f
pandasaurus07 <114919763+pandasaurus07@users.noreply.github.com>
2023-02-01 13:29:01 +0100

    Update parpadeo corazón.py

--- parpadeo corazón.py
+++ parpadeo corazón.py
@@ -1,1 +1,10 @@
+# Imports go at the top
+from microbit import *
 
+imagenes =[Image.HEART_SMALL,Image.HEART]
+
+while True:
+    display.show(Image.HEART_SMALL)
+    sleep(500)
+    display.show(Image.HEART)
+    sleep(500)
